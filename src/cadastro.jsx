c5f7a89e885afb4d79138cc8d27174e990a2858d
Nícolas Figueiredo <nick@MacBook-Air-de-Nicolas.local>
2024-10-03 16:08:07 -0300

    implantacao_axios

--- src/cadastro.jsx
+++ src/cadastro.jsx
@@ -1,11 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import fotoLogin from './img/fotoLogin.jpg'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import fotoLogin from './img/fotoLogin.jpg';
+import axios from 'axios';
+
+
 
 function Cadastro() {
   
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+
+
+
+  const onSubmit = async (ev) => {
+
+    ev.preventDefault();
+    
+    await axios.post('http://127.0.0.1:8000/cadastro', {
+      nome_usuario: nome,
+      email_usuario: email,
+      senha: senha
+    });
+    
+   
+  }
+
+
+
 
   return (
     <>
@@ -17,23 +42,24 @@ function Cadastro() {
         </div>
 
         <div className='divTXT'>
-          
-          <form> 
-            {/* action="/" ->endereço p onde vai   method="POST" */}
-          <h1>Cadastre-se</h1> 
-          <label htmlFor="nome">Nome Completo</label>
-          <input className='inp' name="nome" type="text"></input>
 
-          <label htmlFor="email">E-mail</label>
-          <input className='inp' name="email" type="text"></input>
+        <form onSubmit={onSubmit}>
+          
+            
+          <h1>Cadastre-se</h1> 
+          <label htmlFor="nome_usuario">Nome Completo</label>
+          <input className='inp' name="nome_usuario" value={nome} onChange={(e)=>setNome(e.target.value)} type="text"></input>
+
+          <label htmlFor="email_us">E-mail</label>
+          <input className='inp' name="email_usuario" value={email} onChange={(e)=> setEmail(e.target.value)} type="text"></input>
 
           <label htmlFor="senha">Senha</label>
-          <input className='inp' name="senha" type="password"></input>
+          <input className='inp' name="senha" value={senha} onChange={(e)=>setSenha(e.target.value)} type="text"></input>
 
-          <label htmlFor="senhaConfirma">Confirme sua Senha</label>
-          <input className='inp' name="senha" type="password"></input>
+          {/* <label htmlFor="senhaConfirma">Confirme sua Senha</label>
+          <input className='inp' name="senha" type="password"></input> */}
 
-          <button className='btnEntrar' type="submit" formMethod='POST'>Cadastrar</button>
+          <button className='btnEntrar' type="submit" >Cadastrar</button>
           </form>
           <p>Já possui uma conta? <a href="/"><span>Entre</span></a></p>
         </div>
